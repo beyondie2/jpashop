f7750fa46fec0ab01dc2ab27ac0b1f8c758dd6af
expandsource <expandsource@gmail.com>
2023-11-03 15:53:06 +0900

    장바구니 컨트롤러 예외 처리 및 유효성 검증 추가

--- src/main/java/com/busanit/jpashop/controller/CartController.java
+++ src/main/java/com/busanit/jpashop/controller/CartController.java
@@ -2,15 +2,19 @@ package com.busanit.jpashop.controller;
 
 import com.busanit.jpashop.dto.CartItemDto;
 import com.busanit.jpashop.service.CartService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,15 +25,35 @@ public class CartController {
     // CREATE : 장바구니 담기
     @PostMapping(value = "/cart")
     @ResponseBody
-    public ResponseEntity addCart(@RequestBody CartItemDto cartItemDto, Principal principal) {
-        // 예외처리
+    public ResponseEntity addCart(@RequestBody @Valid CartItemDto cartItemDto, Principal principal, BindingResult bindingResult) {
 
-        // 서비스 위임
+        // 예외처리 : 유효성 검증 에러 발생시 에러 메시지와 함께 400 리턴
+        if (bindingResult.hasErrors()) {
+            // String message = "";
+            StringBuilder message = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                // message += fieldError.getDefaultMessage();
+                message.append(fieldError.getDefaultMessage());
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message.toString());
+        }
+
+
         String email = principal.getName();
-        Long cartItemId = cartService.addCart(cartItemDto, email);
+
+        Long cartItemId;
+        try {
+            // 서비스 위임
+            cartItemId = cartService.addCart(cartItemDto, email);
+        } catch (Exception e) {
+            // 서비스 계층 예외발생시 => 400
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(cartItemId);
     }
+
 
 
 }
